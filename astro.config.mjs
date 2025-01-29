4d95e39de19f9ce3a697f289be063c7c05f49f26
vineyardbovines <spencerfpope@gmail.com>
2025-01-29 17:07:48 -0500

    cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.vineyardbovines.com",
+    site: "https://vineyardbovines.com",
     integrations: [tailwind(), icon()],
     output: "server",
     adapter: node({
