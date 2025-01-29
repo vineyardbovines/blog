765d2da863a60e4abf2c951b25a9355abf4df3c8
vineyardbovines <spencerfpope@gmail.com>
2025-01-29 11:03:24 -0500

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+    site: "https://vineyardbovines.com",
+    integrations: [tailwind(), icon()],
+    output: "server",
+    adapter: node({
+        mode: "standalone",
+    }),
+    markdown: {
+        remarkPlugins: [remarkReadingTime],
+    },
+});
---
+++ src/config.ts
@@ -0,0 +1,156 @@
+import type {
+    NavBarLink,
+    SocialLink,
+    Identity,
+    AboutPageContent,
+    BlogPageContent,
+    HomePageContent,
+} from "./types/config";
+
+export const identity: Identity = {
+    name: "spence pope",
+    logo: "/logo.png",
+    avatar: "/avatar.png",
+    email: "spencer@begaydocrime.tech",
+};
+
+export const navBarLinks: NavBarLink[] = [
+    {
+        title: "home",
+        url: "/",
+    },
+    {
+        title: "about",
+        url: "/about",
+    },
+    {
+        title: "blog",
+        url: "/blog",
+    },
+];
+
+export const socialLinks: SocialLink[] = [
+    {
+        title: "github",
+        url: "https://github.com/vineyardbovines",
+        icon: "mdi:github",
+        external: true,
+    },
+    {
+        title: "email",
+        url: "mailto:spencerfpope@gmail.com",
+        icon: "mdi:email",
+    },
+];
+
+// home (/)
+export const homePageContent: HomePageContent = {
+    seo: {
+        title: "spence pope",
+        description:
+            "Mobile technologist",
+        image: identity.logo,
+    },
+    role: "software engineer",
+    description: "",
+    socialLinks: socialLinks,
+    links: [
+        {
+            title: "about me",
+            url: "/about",
+        },
+    ],
+};
+
+// about (/about)
+export const aboutPageContent: AboutPageContent = {
+    seo: {
+        title: "about | spence pope",
+        description:
+            "mobile technologist",
+        image: identity.logo,
+    },
+    subtitle: "things you may want to know (or not)",
+    about: {
+        description: `
+📱 mobile / iot developer since 2016<br/>
+✌️ 2e autistic since 1992<br/>
+🌲 working remotely from vacationland (maine, usa)<br/>
+🛩️ frequent flyer<br/>
+🐕 3x dog parent to diesel, moose, and hoagie roll<br/>
+🦻 hearing aids wearer<br/>
+🏕️ outdoors enthusiast
+        `,
+        image_l: {
+            url: "/about-horz.jpeg",
+            alt: "spencer with moose and diesel in northern maine, usa",
+        },
+        image_r: {
+            url: "/about-vert.jpeg",
+            alt: "spencer and a hermit crab in saint thomas, us virgin islands",
+        },
+    },
+    work: {
+        description: "i do not have a linkedin, but here's an overview of where I've spent my career.",
+        items: [
+            {
+                title: "Staff Software Engineer",
+                company: {
+                    name: "Going (fka Scott's Cheap Flights)",
+                    image: "/going.png",
+                    url: "https://going.com",
+                },
+                date: "2021 - Present",
+            },
+            {
+                title: "Senior Software Engineer",
+                company: {
+                    name: "Very",
+                    image: "/very.png",
+                    url: "https://www.verytechnology.com/",
+                },
+                date: "2020 - 2021",
+            },
+            {
+                title: "Senior Software Engineer",
+                company: {
+                    name: "MedRhythms",
+                    image: "/mr.png",
+                    url: "https://www.medrhythms.com/",
+                },
+                date: "2018 - 2020",
+            },
+            {
+                title: "Lead Software Engineer",
+                company: {
+                    name: "Freight Farms",
+                    image: "/ff.png",
+                    url: "https://www.freightfarms.com/",
+                },
+                date: "2017 - 2028",
+            },
+            {
+                title: "Software Developer",
+                company: {
+                    name: "Ideometry",
+                    image: "/ideo.png",
+                    url: "https://www.ideometry.com/",
+                },
+                date: "2016 - 2017",
+            },
+        ],
+    },
+    connect: {
+        description: `feel free to connect with me on any of the following platforms.`,
+        links: socialLinks,
+    },
+};
+
+// blog (/blog)
+export const blogPageContent: BlogPageContent = {
+    seo: {
+        title: "blog | spence pope",
+        description: "unsolicited thoughts and opinions",
+    },
+    subtitle: "unsolicited thoughts and opinions",
+};
