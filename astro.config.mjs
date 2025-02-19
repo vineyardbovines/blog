c0c744a6036edfec753bd904dae12283f5cc567c
vineyardbovines <spencerfpope@gmail.com>
2025-02-18 19:06:25 -0500

    ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import node from "@astrojs/node";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://thepope.dev",
+  output: "server",
+  adapter: cloudflare(),
   integrations: [tailwind(), icon()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
